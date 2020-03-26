f65094f2014ec280c07e75b9597eb86fe1bbc1b5
LiegardP <pierre.liegard@gmail.com>
2020-03-26 10:45:02 +0100

    log percentage difference between 2 reports + add color to shell

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const argv = require('yargs').argv; // to easily pass arguments in terminal
 const url = require('url'); // to deal with url in node
 const fs = require('fs'); // to deal with file system (create dir)
 const glob = require('glob'); // for matching file
-const path = require('path');
+const path = require('path'); // to deal with path in node
 
 // function to launch chrome with url param
 // return a promise 
@@ -33,9 +33,54 @@ const getContents = pathStr => { // path of json file
     return JSON.parse(output); // return content of specific file
 };
 
-const compareReports = (from, to) => {
-    console.log(from["finalUrl"] + " " + from["fetchTime"]);
-    console.log(to["finalUrl"] + " " + to["fetchTime"]);
+const compareReports = (from, to) => { // compare 2 reports with metrics below
+    const metricFilter = [
+        "first-contentful-paint",
+        "first-meaningful-paint",
+        "speed-index",
+        "estimated-input-latency",
+        "total-blocking-time",
+        "max-potential-fid",
+        "time-to-first-byte",
+        "first-cpu-idle",
+        "interactive"
+    ];
+
+    // to calculate the percentage diff, use this formula => ((From - To) / From) x 100
+    const calcPercentageDiff = (from, to) => {
+        const per = ((from - to) / ((from + to) / 2) * 100);
+        // const per = ((to - from) / from) * 100;
+        return Math.round(per * 100) / 100;
+    };
+
+    // Loop through audits and check if they have value in metricFilter
+    for (let auditObj in from["audits"]) {
+        if (metricFilter.includes(auditObj)) { // if metrics is present, calculate percentage diff
+            const percentageDiff = calcPercentageDiff(
+                from["audits"][auditObj].numericValue,
+                to["audits"][auditObj].numericValue
+            );
+
+            // log with color in terminal when launching a new report
+            let logColor = "\x1b[37m";
+            const log = (() => {
+              if (Math.sign(percentageDiff) === 1) {
+                logColor = "\x1b[31m";
+                return `${percentageDiff + "%"} slower`;
+              } else if (Math.sign(percentageDiff) === 0) {
+                return "unchanged";
+              } else {
+                logColor = "\x1b[32m";
+                return `${percentageDiff + "%"} faster`;
+              }
+            })();
+            console.log(logColor, `${from["audits"][auditObj].title} is ${log}`);
+          }
+
+        }
+
+    console.log('\x1b[36m%s\x1b[0m', 'compare this report : ' + from["finalUrl"] + " " + from["fetchTime"]);
+    console.log('\x1b[36m%s\x1b[0m', 'to this report' + to["finalUrl"] + " " + to["fetchTime"]);
 };
 
 // launch function
